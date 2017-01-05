c9986e0d8b7fe674cdb5e0fd211c7d3ba87cfa5c
Mina Jafari <mjafari@umich.edu>
2017-01-05 16:53:44 -0500

    Read from new input file structure

--- source-code/align-structures/main.cpp
+++ source-code/align-structures/main.cpp
@@ -12,93 +12,78 @@
 #include <fstream>
 #include <stdio.h>
 
-
 // slab, atom1, .. : Surface
 // adsorbate1, atom2, .. : Adsorbate-1
 
 bool populateArrayFromVector(std::vector<std::string> inVec, double* inArr, int numOfAtoms, 
                  std::string* inSymbols);
-bool readFromFile(std::string inFileName, int &numOfAdd, int* addArray);
+//bool readFromFile(std::string inFileName, int &numOfAdd, int* addArray);
+//bool readFromFile(std::string inFileName, vector<std::string>& xyzFileSlab);
+//bool readFromFile(std::string inFileName);
+int readFromFile(std::string inFileName, int &numOfAdsorbates, int* slabIndices, double* radius,
+                  std::string* adsorbateFiles, int* adsorbateIndices, int* reactiveIndex1,
+                  int* reactiveIndex2, int &numOfAdd, int &numOfBreak);
+
+//bool readSlabFile(std::string slabFileName, vector<std::string>& xyzFileSlab)
+bool readSlabFile(std::string &slabFileName);
 
 int main(int argc, char* argv[])
 {
-    std::vector<std::string> xyzFileSlab; // vector to store the input file
-    if (argc < 3 || argc > 4)
+    //if (argc < 3 || argc > 4)
+
+    // list of adsorbate and slab indices
+    int addArray[4] = {};
+
+    if (argc != 1)
     {   
         std::cout << "ERROR - wrong number of parameters" << std::endl;
-        std::cout << "Usage: " << argv[0] << " <slab file> " 
-                  << "<adsorbate-1 file> " << "<adsorbate-2 file>" << std::endl;
+        std::cout << "Usage: " << argv[0] << " " << std::endl;
         return (1);
-    }   
-    else // reading the input file
-    std::cout << "Reading input files" << std::endl;
-    {   
-        std::ifstream inFile (argv[1]);
-        std::string newLine;
-        while (std::getline(inFile, newLine))
-        {   
-            xyzFileSlab.push_back(newLine);
-        }   
-    }   
-    int numOfSlabAtoms = std::stoi(*(xyzFileSlab.begin()));
-    std::string surfaceType = xyzFileSlab[1];
-    if (xyzFileSlab[1].empty())
-    {   
-        std::cout << "ERROR: Set surface type in the input file" << std::endl;
-    }   
-
-    int slabSize = 3 * numOfSlabAtoms;
-    double* slabCartesianCoords = new double[slabSize];
-    std::string* slabAtomicSymbols = new std::string[numOfSlabAtoms];
-
-    populateArrayFromVector(xyzFileSlab, slabCartesianCoords, numOfSlabAtoms, slabAtomicSymbols);
-    SurfaceClass aSurface;
-    std::string outFName = "bindingSites.xyz";
-    if (aSurface.setSurfaceType(surfaceType))
-    {   
-        if (!aSurface.setAtoms(numOfSlabAtoms, slabCartesianCoords, slabAtomicSymbols))
-        {   
-            std::cout << "ERROR: " << std::endl;
-            return (2);
-        }   
-        aSurface.findAllSites();
-        aSurface.writeToFile(outFName);
-    } 
-    // create objects from input structures
-    ICoord slab, adsorbate1, adsorbate2;
-    std::vector<ICoord> adsorbates;
-    if (argc == 3)
-    {
-        slab.init(outFName);
-        adsorbate1.init(argv[2]);
-        adsorbates.push_back(adsorbate1);
     }
-    else if (argc == 4)
+    // reading input files
+    else
     {
-        slab.init(outFName);
-        adsorbate1.init(argv[2]);
-        adsorbate2.init(argv[3]);
+        std::cout << "Reading input files" << std::endl;
+        int numOfAdsorbates = 0;
+        int slabIndices[2] = {};
+        double radius[2] = {};
+        std::string adsorbateFiles[2] = {};
+        int adsorbateIndices[2] = {};
+        int reactiveIndex1[4] = {0, 0, 0, 0}; //maximum of 4 reactive atoms on each atom
+        int reactiveIndex2[4] = {0, 0, 0, 0};
+        int numOfAdd = 0;
+        int numOfBreak = 0;
+
+        int returnVal = 0;
+        returnVal = readFromFile("INPUT", numOfAdsorbates, slabIndices, radius, adsorbateFiles, 
+                     adsorbateIndices, reactiveIndex1, reactiveIndex2,
+                     numOfAdd, numOfBreak);
+        if (returnVal == 0)
+        {
+        // create objects from input structures
+        ICoord slab, adsorbate1, adsorbate2;
+        std::vector<ICoord> adsorbates;
+
+        slab.init("bindingSites.xyz");
+        adsorbate1.init(adsorbateFiles[0]);
         adsorbates.push_back(adsorbate1);
-        adsorbates.push_back(adsorbate2);
-    }
+        if (numOfAdsorbates == 2)
+        {
+            adsorbate2.init(adsorbateFiles[1]);
+            adsorbates.push_back(adsorbate2);
+        }
+
     Align totalSystem(slab, adsorbates);
 
     // read parameters from INPUT file
-    int numOfAdd = 0;
-    int addArray[4] = {};
     //std::string orientationIn = "horiz";
-
-    readFromFile("INPUT", numOfAdd, addArray);
-    totalSystem.add_align(numOfAdd, addArray); /*orientationIn//default is horiz*/
-    std::cout << "\n***************************************\n";
-    std::cout << "\nOutput is written to aligned-*.xyz file\n";
-    std::cout << "\n***************************************\n";
-    
-    delete [] slabCartesianCoords;
-    delete [] slabAtomicSymbols;
-    //slab.freemem();
-    //adsorbate1.freemem();
-    //adsorbate2.freemem();
+    // numOfAdsorbates = numOfAdd passed to add_align
+//    totalSystem.add_align(numOfAdsorbates, addArray); /*orientationIn//default is horiz*/
+//    std::cout << "\n***************************************\n";
+//    std::cout << "\nOutput is written to Aligned-*.xyz file\n";
+//    std::cout << "\n***************************************\n";
+        }
+    }
     return (0);
 }
 
@@ -132,9 +117,11 @@ bool populateArrayFromVector(std::vector<std::string> inVec, double* inArr, int 
     return (success);
 }
 
-bool readFromFile(std::string inFileName, int &numOfAdd, int* addArray)
+int readFromFile(std::string inFileName, int &numOfAdsorbates, int* slabIndices, double* radius,
+                  std::string* adsorbateFiles, int* adsorbateIndices, int* reactiveIndex1,
+                  int* reactiveIndex2, int &numOfAdd, int &numOfBreak)
 {
-    bool success = false;
+    // populate vector by input file lines
     std::vector<std::string> inputFile;
     std::string newLine;
     ifstream inFile;
@@ -142,37 +129,142 @@ bool readFromFile(std::string inFileName, int &numOfAdd, int* addArray)
     while (std::getline(inFile, newLine))
     {
         inputFile.push_back(newLine);
+        //std::cout << newLine << std::endl;
     }
 
+    // start parsing input file
     std::stringstream ss(*inputFile.begin());
     std::string unwanted = "";
     std::string temp[2] = {};
-    //if (ss.peek() != "#")
-    ss >> unwanted >> numOfAdd;
-    if (numOfAdd == 1)
+    
+    std::string findSites = "";
+    ss >> unwanted >> findSites >> unwanted;
+    std::string slabFileName = "";
+
+    if (std::stoi(findSites) == 1)
     {
-        ss.str("");
-        ss.clear();
+        // stage 1: find binding sites
+        std::cout << "** Finding binding sites" << std::endl;
         ss.str(*(inputFile.begin()+1));
-        ss >> unwanted >> unwanted >> temp[0] >> temp[1];
-        addArray[0] = std::stoi(temp[0]);
-        addArray[1] = std::stoi(temp[1]);
+        ss >> unwanted >> slabFileName >> unwanted;
+        readSlabFile(slabFileName);
+        return 1;
     }
-    else if (numOfAdd == 2)
+    else if (std::stoi(findSites) == 0)
     {
-        ss.str("");
-        ss.clear();
-        ss.str(*(inputFile.begin()+1));
-        ss >> unwanted >> unwanted >> temp[0] >> temp[1];
-        addArray[0] = std::stoi(temp[0]);
-        addArray[1] = std::stoi(temp[1]);
-        ss.str("");
+        // stage 2: add adsorbates to binding sites
+        std::cout << "** reading input file information" << std::endl;
+        std::cout << "** WARNING: slab is read from bindingSites.xyz" << std::endl;
+        // line 3 of INPUT file
         ss.clear();
         ss.str(*(inputFile.begin()+2));
-        ss >> unwanted >> unwanted >> temp[0] >> temp[1];
-        addArray[2] = std::stoi(temp[0]);
-        addArray[3] = std::stoi(temp[1]);
+        ss >> unwanted >> temp[0] >> unwanted;
+        numOfAdsorbates = std::stoi(temp[0]);
+        // line 4
+        ss.str(*(inputFile.begin()+3));
+        ss >> unwanted >> temp[0] >> unwanted;
+        slabIndices[0] = std::stoi(temp[0]);
+        // line 5
+        ss.str(*(inputFile.begin()+4));
+        ss >> unwanted >> temp[0] >> unwanted;
+        radius[0] = std::stod(temp[0]);
+        // line 6
+        ss.str(*(inputFile.begin()+5));
+        ss >> unwanted >> adsorbateFiles[0] >> unwanted;
+        // line 7
+        ss.str(*(inputFile.begin()+6));
+        ss >> unwanted >> temp[0] >> unwanted;
+        adsorbateIndices[0] = std::stoi(temp[0]); // TODO + num of slab atoms
+        // line 8
+        ss.str(*(inputFile.begin()+7));
+        ss >> unwanted >> temp[0] >> unwanted;
+        reactiveIndex1[0] = std::stoi(temp[0]);
+
+        if (numOfAdsorbates == 2)
+        {
+            // lines 9-14
+            ss.str(*(inputFile.begin()+9));
+            ss >> unwanted >> temp[0] >> unwanted;
+            slabIndices[1] = std::stoi(temp[0]);
+            ss.str(*(inputFile.begin()+10));
+            ss >> unwanted >> temp[0] >> unwanted;
+            radius[1] = std::stod(temp[0]);
+            ss.str(*(inputFile.begin()+11));
+            ss >> unwanted >> adsorbateFiles[1] >> unwanted;
+            ss.str(*(inputFile.begin()+12));
+            ss >> unwanted >> temp[0] >> unwanted;
+            adsorbateIndices[1] = std::stoi(temp[0]); // TODO + num of slab+adsorbate1 atoms
+            ss.str(*(inputFile.begin()+13));
+            ss >> unwanted >> temp[0] >> temp[1] >> unwanted; //TODO
+            reactiveIndex2[0] = std::stoi(temp[0]);
+            reactiveIndex2[1] = std::stoi(temp[1]);
+        }
+
+        ss.str(*(inputFile.begin()+15));
+        ss >> unwanted >> temp[0] >> unwanted;
+        numOfAdd = std::stoi(temp[0]);
+        ss.str(*(inputFile.begin()+16));
+        ss >> unwanted >> temp[0] >> unwanted;
+        numOfBreak = std::stoi(temp[0]);
+
+        std::cout << numOfAdsorbates << "   " << slabIndices[0] << "    " << slabIndices[1] 
+            << "    " << radius[0] << "     " << radius[1] << "     " << adsorbateFiles[0]
+            << "    " << adsorbateFiles[1] << " " << adsorbateIndices[0] << "   " <<
+            adsorbateIndices[1] << "    " << numOfAdd
+            << "    " << numOfBreak << "   " << reactiveIndex1[0] << "     " << reactiveIndex2[0] << std::endl;
+    return 0;
     }
+}
+
+bool readSlabFile(std::string &slabFileName)
+{
+    bool success = false;
+    std::vector<std::string> xyzFileSlab; // vector to store the input file
+    std::ifstream inFile;
+    inFile.open(slabFileName);
+    if (!inFile.is_open())
+    {
+        std::cout << "ERROR: File " << slabFileName << " does not exist!" << std::endl;
+    }
+    std::string newLine;
+    while (std::getline(inFile, newLine))
+    {   
+        xyzFileSlab.push_back(newLine);
+    }   
+
+    int numOfSlabAtoms = std::stoi(*(xyzFileSlab.begin()));
+    std::string surfaceType = xyzFileSlab[1];
+    if (xyzFileSlab[1].empty())
+    {   
+        std::cout << "ERROR: Set surface type in the input file" << std::endl;
+    }   
+
+    int slabSize = 3 * numOfSlabAtoms;
+    double* slabCartesianCoords = new double[slabSize];
+    std::string* slabAtomicSymbols = new std::string[numOfSlabAtoms];
+
+    populateArrayFromVector(xyzFileSlab, slabCartesianCoords, numOfSlabAtoms, slabAtomicSymbols);
+    SurfaceClass aSurface;
+    std::string outFName = "bindingSites.xyz";
+    if (aSurface.setSurfaceType(surfaceType))
+    {   
+        if (!aSurface.setAtoms(numOfSlabAtoms, slabCartesianCoords, slabAtomicSymbols))
+        {   
+            std::cout << "ERROR: " << std::endl;
+            return (2);
+        }   
+        aSurface.findAllSites();
+        aSurface.writeToFile(outFName);
+        success = true;
+    }
+
+    delete [] slabCartesianCoords;
+    delete [] slabAtomicSymbols;
 
     return success;
 }
+
+
+
+
+
